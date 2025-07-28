2c6acdf92202693a51216d00db64d48f5f6023b8
Aaron Curtis <foobarbecue@gmail.com>
2025-07-27 20:54:15 -0700

    Add node script for ingesting soloshot files

---
+++ ingest/ingest_seshfiles.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs')
+const path = require('node:path')
+const {Glob} = require('glob')
+
+const [,, soloshotSdDir, allwavesCheckout] = process.argv
+
+const seshFileSdPaths = new Glob(`${soloshotSdDir}/Track_*/*.SESSION`, {})
+const seshFileGhDirPath = `${allwavesCheckout}/seshfiles/orig/`
+
+
+for (seshFilePath of seshFileSdPaths) {
+    const destFilePath = path.join(seshFileGhDirPath, path.basename(seshFilePath))
+    console.log(`Copying ${seshFilePath} to ${destFilePath}`)
+    fs.cpSync(seshFilePath, destFilePath)
+    const symlinkName = destFilePath.replace(/\\?orig/,'').replace(/_\d{6}/,'')
+
+    //TODO handle multiple sessions on same day
+    console.log(`Creating symlink ${symlinkName} pointed to ${destFilePath}`)
+    fs.symlinkSync(destFilePath, symlinkName)
+}
+
