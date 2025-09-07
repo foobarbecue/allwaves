093f6776d5330994caf57a6b08e8a54beaaad9bd
Aaron Curtis <foobarbecue@gmail.com>
2025-09-07 16:33:32 -0700

    plot tags separately

--- ingest/find_waves.mjs
+++ ingest/find_waves.mjs
@@ -3,47 +3,57 @@ import * as fs from 'node:fs';
 import tc from 'transform-coordinates';
 import { plot } from 'nodeplotlib';
 
-// Parse session
-const seshFileData = fs.readFileSync('C:\\Users\\aaron\\IdeaProjects\\allwaves\\seshfiles\\orig\\SS3_EDIT_2025_08_17_083422.SESSION')
-const sesh = await parseSession(seshFileData);
-
-// Convert to meters in UTM 11N
-const transform = tc('EPSG:4326', 'EPSG:32611');
-
-const tagLocnsUTM = sesh.locations.map(
-    locn => transform.forward(
-        {x: locn.tagPosition.latitude, y: locn.tagPosition.longitude, z: locn.tagPosition.elevation}
+const plotTag = (tagId, sesh)=> {
+    const tagSesh = sesh.locations.filter(
+        (datum)=>datum.tagId == tagId
     )
-)
+    const moveDists = [];
+    const moveDurations = [];
+    const moveSpeeds = [];
+    const tagLocnsUTM = tagSesh.map(
+        locn => transform.forward(
+            {x: locn.tagPosition.latitude, y: locn.tagPosition.longitude, z: locn.tagPosition.elevation}
+        )
+    )
+    for (let ind = 0; ind < tagLocnsUTM.length - 1; ind++) {
+        moveDists.push(
+            Math.sqrt((tagLocnsUTM[ind + 1].x - tagLocnsUTM[ind].x) ** 2 + (tagLocnsUTM[ind + 1].y - tagLocnsUTM[ind].y) ** 2)
+        );
+        moveDurations.push(
+            tagSesh[ind + 1].timestamp - tagSesh[ind + 1].timestamp
+        );
+        moveSpeeds.push(
+            moveDurations[ind] / moveDists[ind]
+        );
+    }
 
-const moveDists = [];
-const moveDurations = [];
-const moveSpeeds = [];
-for (let ind=0; ind < tagLocnsUTM.length-1; ind++) {
-    moveDists.push(
-        Math.sqrt((tagLocnsUTM[ind + 1].x - tagLocnsUTM[ind].x) ** 2 + (tagLocnsUTM[ind + 1].y - tagLocnsUTM[ind].y) ** 2)
-    );
-    moveDurations.push(
-        sesh.locations[ind+1].timestamp - sesh.locations[ind+1].timestamp
-    );
-    moveSpeeds.push(
-        moveDurations[ind] / moveDists[ind]
-    );
-}
-
-const distsPlot = [{
+    const distsPlot = [{
         y: moveDists,
         x: [...Array(moveDists.length).keys()],
         type: 'scatter'
     }]
 
-const locnPlot = [{
+    const locnPlot = [{
         y: tagLocnsUTM.map(locn => locn.y),
         x: tagLocnsUTM.map(locn => locn.x)
-}]
+    }]
 
-plot(distsPlot)
-plot(locnPlot)
+    plot(distsPlot)
+    plot(locnPlot)
 // console.log(transform.forward(seshDf.select(tagPosition,0)));
 // Calculate speed
 
+}
+
+// Parse session
+const seshFileData = fs.readFileSync('C:\\Users\\aaron\\IdeaProjects\\allwaves\\seshfiles\\orig\\SS3_EDIT_2025_08_17_083422.SESSION')
+const sesh = await parseSession(seshFileData);
+
+// find all tag ids
+const tagIds = sesh.locations.map(locn => locn.tagId)
+const tagIdsSet = new Set(tagIds)
+
+// Convert to meters in UTM 11N
+const transform = tc('EPSG:4326', 'EPSG:32611');
+
+tagIdsSet.forEach((tagId)=>{plotTag(tagId, sesh);})
