db14db72e5c7281e46b52b9114ef785dcb446304
Aaron Curtis <foobarbecue@gmail.com>
2025-11-08 14:37:43 -0800

    make generated symlinks relative

--- cli_tools/ingest_seshfiles.js
+++ cli_tools/ingest_seshfiles.js
@@ -4,7 +4,7 @@ const {Glob} = require('glob')
 
 function printHelp() {
     const script = path.basename(__filename)
-    console.log(`Usage: node cli_tools\\${script} <SOLOSHOT_SD_DIR> <ALLWAVES_CHECKOUT_DIR>\n\nCopies *.SESSION files from a Soloshot SD card into the allwaves repo and creates a convenient symlink without the date suffix.\n\nArguments:\n  <SOLOSHOT_SD_DIR>         Path to the Soloshot SD card root (expects Track_*/ directories).\n  <ALLWAVES_CHECKOUT_DIR>   Path to the allwaves repository checkout.\n\nOptions:\n  -h, --help                Show this help and exit.`)
+    console.log(`Usage: node cli_tools\\${script} <SOLOSHOT_SD_DIR> <ALLWAVES_CHECKOUT_DIR>\n\nCopies *.SESSION files from a Soloshot SD card into the allwaves repo and creates a convenient symlink without the date suffix.\n\nArguments:\n  <SOLOSHOT_SD_DIR>         Path to the Soloshot SD card root (expects a SOLOSHOT3 directory).\n  <ALLWAVES_CHECKOUT_DIR>   Path to the allwaves repository checkout.\n\nOptions:\n  -h, --help                Show this help and exit.`)
 }
 
 const args = process.argv.slice(2)
@@ -31,11 +31,12 @@ for (seshFilePath of seshFileSdPaths) {
     console.log(`Copying ${seshFilePath} to ${destFilePath}`)
     fs.cpSync(seshFilePath, destFilePath)
     const symlinkName = destFilePath.replace(/\\?orig/,'').replace(/_\d{6}/,'')
+    const origFileRelPath = destFilePath.replace(/^.*seshfiles/,'.')
 
     //TODO handle multiple sessions on same day
     try {
-        console.log(`Creating symlink ${symlinkName} pointed to ${destFilePath}`)
-        fs.symlinkSync(destFilePath, symlinkName)
+        console.log(`Creating symlink ${symlinkName} pointed to ${origFileRelPath}`)
+        fs.symlinkSync(origFileRelPath, symlinkName)
     } catch (err) {
         console.error(err)
     }
