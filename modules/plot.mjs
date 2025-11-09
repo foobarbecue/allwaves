a4a48c6b9dd1eea7bc643ca969bb41dc7a707ee8
Aaron Curtis <foobarbecue@gmail.com>
2025-11-09 15:07:33 -0800

    plot title and tick format

--- modules/plot.mjs
+++ modules/plot.mjs
@@ -2,8 +2,10 @@ import "https://cdn.plot.ly/plotly-3.1.2.min.js"
 import {loadAndParseSession} from "./parse_session.mjs";
 import {getClosestIndex} from "./math.mjs";
 import proj4 from 'https://cdn.jsdelivr.net/npm/proj4@2.9.2/+esm'
+import {seshDate, seshTimestampCache, seshGeodataCache} from "./data.js";
 
 
+// TODO use cache
 const getTagCoordsForPlot = (tagId, sesh) => {
     const tagSesh = sesh.locations.filter(
         (datum) => datum.tagId == tagId
@@ -11,6 +13,7 @@ const getTagCoordsForPlot = (tagId, sesh) => {
     const moveDists = [];
     const moveDurations = [];
     const moveSpeeds = [];
+    const moveTimes = [];
     const tagLocnsUTM = tagSesh.map(
         // Convert to meters in UTM 11N
         locn => proj4(
@@ -28,11 +31,17 @@ const getTagCoordsForPlot = (tagId, sesh) => {
         moveSpeeds.push(
             moveDists[ind] / (moveDurations[ind] * 1000)
         );
+        moveTimes.push(
+            tagSesh[ind].timestamp
+        )
     }
+
+    document.querySelector("#wave-plot-title").textContent = `Mapping: ${seshDate}`;
 
     const distsPlot = [{
         y: moveDists,
-        x: [...Array(moveDists.length).keys()]
+        x: moveTimes.map(timestamp => new Date(timestamp)),
+        name: `Tag ${tagId}: speed`
     }]
 
     // const locnPlot = {
@@ -53,8 +62,8 @@ export async function plotSession(seshDate) {
     const tagIdsSet = new Set(tagIds)
 
     const locnPlot = getTagCoordsForPlot(tagIdsSet.entries().toArray()[0][0], sesh)
-
-    await Plotly.newPlot('wave-plot', locnPlot)
+    const layout = {title: {text: 'Speed'}, showLegend: true, xaxis:{tickformat: '%H:%M'}}
+    await Plotly.newPlot('wave-plot', locnPlot, layout)
 }
 
 export function setTimebarToTime(timelist, time){
@@ -66,7 +75,7 @@ export function setTimebarToTime(timelist, time){
         return
     }
 
-    const x = Number(wptTimeIdx)
+    const x = Number(timelist[wptTimeIdx])
     const timebar = {
         type: 'line',
         x0: x,
